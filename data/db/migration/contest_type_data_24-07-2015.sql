251e90f51259d10803a55491bc4f20d0bcc683dd
sumit <sumit@riktamtech.com>
2015-07-24 13:30:51 +0530

    contest type data

---
+++ data/db/migration/contest_type_data_24-07-2015.sql
@@ -0,0 +1,4 @@
+INSERT INTO `contest_type` (`id`, `type`, `created_at`, `updated_at`) VALUES
+(1, 'Photo Contest', '2015-07-21 00:00:00', '2015-07-21 00:00:00'),
+(2, 'Fan Favorite', '2015-07-21 00:00:00', '2015-07-21 00:00:00'),
+(3, 'Brackets', '2015-07-21 00:00:00', '2015-07-21 00:00:00');
