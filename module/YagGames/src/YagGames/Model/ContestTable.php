8b2a95b187cc5670f550c8c4b239ef21058ed4c6
vishwa <vishwa@riktamtech.com>
2015-08-12 12:58:09 +0530

    description changes

--- module/YagGames/src/YagGames/Model/ContestTable.php
+++ module/YagGames/src/YagGames/Model/ContestTable.php
@@ -400,11 +400,8 @@ class ContestTable extends BaseTable {
     
     public function getByContestId($contestId) {        
         if ($contestId) {
-            $contestSql = $this->getSql()->select()
-                    ->from(array('c' => 'contest'))
-                    ->join(array('ct' => 'contest_type'), 'ct.id = c.type_id', array('type'))
-                    ->columns(array('*'))   
-                    ->where(array('c.id' => $contestId));
+            $contestSql =  $this->getContestBaseSelect();
+            $contestSql->where(array('c.id' => $contestId));
         }
         
         $statement = $this->getSql()->prepareStatementForSqlObject($contestSql);        
