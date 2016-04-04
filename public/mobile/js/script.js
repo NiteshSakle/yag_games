31c30288fcf5b63262b538427ae685d85c23f22b
Mahesh <mahesh@riktamtech.com>
2016-04-04 18:45:29 +0530

    Voting, Ranking pages changes

--- public/mobile/js/script.js
+++ public/mobile/js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
     $('.show-round-btn').on('click', function(){
         var displaySection = $(this).data('show-section');        
         if ($(displaySection).length) {
-            $('div[id=r1Section], div[id=r2Section], div[id=r3Section], div[id=r4Section], div[id=r5Section], div[id=r6Section]').hide();
+            $('div#r1Section, div#r2Section, div#r3Section, div#r4Section, div#r5Section, div#r6Section, div#championSection').hide();
             $(displaySection).show();
              $("html, body").animate({scrollTop: $(displaySection).offset().top - 100}, "slow");
         }
@@ -17,6 +17,21 @@ $(document).ready(function(){
         var mediaId = $(this).data('mediaid');
         if (mediaId.length) {
            window.location.href = baseURL+'/photo/'+mediaId+'/photo.html'; 
+        }
+    });
+    
+    $('.bracket-rankings').swipe({
+        swipeLeft: function () {            
+            $('.bracket-rankings').find('.swipe-left:visible').trigger('click');            
+        },
+        swipeRight: function () {
+            $('.bracket-rankings').find('.swipe-right:visible').trigger('click');
+        }
+    });
+    
+    $('a.swipe-left, a.swipe-right').on('click', function(){
+        if($(this).attr('href').length)  {
+            window.location.href = $(this).attr('href');
         }
     });
 });
